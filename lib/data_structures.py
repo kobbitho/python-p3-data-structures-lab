3a695e68e15ecb6055d4ca626d78f705eec86ca2
Tabithaokoth <kobbitho@gmail.com>
2024-05-24 09:22:19 +0300

    adds new lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,61 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    return[food["name"] for food in spicy_foods]
+food_names = get_names(spicy_foods)
+#print(food_names)
+    
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
+spiciest_foods = get_spiciest_foods(spicy_foods)
+#print(spiciest_foods)
+
+    
 def print_spicy_foods(spicy_foods):
-    pass
+   for food in spicy_foods:
+        heat_level = ""
+        for _ in range(food["heat_level"]):
+            heat_level += "🌶"
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+
+#print_spicy_foods(spicy_foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    return next((food for food in spicy_foods if food["cuisine"]==cuisine), None)
+#print(get_spicy_food_by_cuisine(spicy_foods, "American"))
+
+
 
 def print_spiciest_foods(spicy_foods):
-    pass
+     for food in get_spiciest_foods(spicy_foods):
+        heat_level = ""
+        for _ in range(food["heat_level"]):
+            heat_level += "🌶"
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+
+#print_spiciest_foods(spicy_foods)
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat_level = sum(food["heat_level"] for food in spicy_foods)
+    return total_heat_level / len(spicy_foods)
+
+#print(get_average_heat_level(spicy_foods))
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    new_spicy_food = {
+        "name": spicy_food["name"],
+        "cuisine": spicy_food["cuisine"],
+        "heat_level": spicy_food["heat_level"]
+    }
+    spicy_foods.append(new_spicy_food)
+    return spicy_foods
+
+spicy_foods = create_spicy_food(spicy_foods, {
+    'name': 'Griot',
+    'cuisine': 'Haitian',
+    'heat_level': 10,
+})
+#print(spicy_foods)
